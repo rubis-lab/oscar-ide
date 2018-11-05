43b9b51189e3e1e77c1fd6addcb09bc98ae81875
Youngeun Cho <upoque@gmail.com>
2018-11-05 22:51:18 +0900

    OSCARIDE-28 GEF integration implemented

--- bundles/org.rubis.oscar.rcp/src/org/rubis/oscar/rcp/gef/editor/OSCARGraphicalEditor.java
+++ bundles/org.rubis.oscar.rcp/src/org/rubis/oscar/rcp/gef/editor/OSCARGraphicalEditor.java
@@ -1,16 +1,15 @@
-package org.rubis.oscar.oscarcore.gef.editor;
-
-import org.eclipse.core.runtime.IProgressMonitor;
+package org.rubis.oscar.rcp.gef.editor;
 
 import org.eclipse.gef.DefaultEditDomain;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.gef.palette.PaletteRoot;
 import org.eclipse.gef.ui.parts.GraphicalEditorWithFlyoutPalette;
 
 public class OSCARGraphicalEditor extends GraphicalEditorWithFlyoutPalette {
 
 	public OSCARGraphicalEditor() {
-		setEditDomain(new DefaultEditDomain(this));
-	}
+	    setEditDomain(new DefaultEditDomain(this));
+	  }
 	
 	@Override
 	protected PaletteRoot getPaletteRoot() {
