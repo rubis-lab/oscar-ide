ce5d79887e9f8609653ca7a0a3c6b64f91e74b3f
Do Hyung Kim <19693586+dohkim119@users.noreply.github.com>
2018-11-05 21:46:58 +0900

    OSCARIDE-30 Add project navigation template

---
+++ bundles/org.rubis.oscar.rcp/src/org/rubis/oscar/rcp/ApplicationWorkbenchWindowAdvisor.java
@@ -0,0 +1,26 @@
+package org.rubis.oscar.rcp;
+
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.ui.application.ActionBarAdvisor;
+import org.eclipse.ui.application.IActionBarConfigurer;
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
+
+    public ApplicationWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+        super(configurer);
+    }
+
+    public ActionBarAdvisor createActionBarAdvisor(IActionBarConfigurer configurer) {
+        return new ApplicationActionBarAdvisor(configurer);
+    }
+    
+    public void preWindowOpen() {
+        IWorkbenchWindowConfigurer configurer = getWindowConfigurer();
+        configurer.setInitialSize(new Point(2000, 1500));
+        configurer.setShowCoolBar(false);
+        configurer.setShowStatusLine(false);
+        configurer.setTitle("OSCAR IDE(alpha)"); //$NON-NLS-1$
+    }
+}
